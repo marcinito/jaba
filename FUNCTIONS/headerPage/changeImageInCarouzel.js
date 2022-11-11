a5619738aeb170f094f20474c55e2ede256173bf
Marcin <marcin@o2.pl>
2022-11-11 18:12:26 +0000

    before make advance effect in slider

--- FUNCTIONS/headerPage/changeImageInCarouzel.js
+++ FUNCTIONS/headerPage/changeImageInCarouzel.js
@@ -1,10 +1,17 @@
 let counter=1
 let speedTransition=1
-export const changeImageInCarouzel=(slideRef,whichButton,imgSlider)=>{
+export const changeImageInCarouzel=(slideRef,whichButton,imgSlider,effect)=>{
 
     let size=slideRef.current.clientWidth
-    
+    //LEFT BUTTON
 if(whichButton==="left"){
+
+    effect.current.style.transition=`0.5s`
+    effect.current.style.transform=`translateX(200%)`
+    setTimeout(()=>{
+        effect.current.style.transition=`0s`
+    effect.current.style.transform=`translateX(-100%)`
+    },510)
     counter--
     if(counter===0){
         counter=2+imgSlider.length-1
@@ -22,7 +29,14 @@ if(whichButton==="left"){
     slideRef.current.style.transform=`translateX(`+ -size * counter + "px"
     slideRef.current.style.transition=`transform ${speedTransition}s`
 }
+//RIGHT BUTTON
 if(whichButton==="right"){
+effect.current.style.transition=`0.5s`
+effect.current.style.transform=`translateX(-100%)`
+setTimeout(()=>{
+    effect.current.style.transition=`0s`
+effect.current.style.transform=`translateX(200%)`
+},510)
     counter++
     if(counter===2+imgSlider.length-1){
         counter=0
--- component/HeaderPage.js
+++ component/HeaderPage.js
@@ -2,10 +2,29 @@ import { useEffect, useRef, useState } from 'react';
 import s from '../styles/Header.module.scss'
 import { changeImageInCarouzel } from '../FUNCTIONS/headerPage/changeImageInCarouzel';
 const HeaderPage = ({data}) => {
-   
+    const effectSlideFn=(e)=>{
+     
+     
+            let width=window.innerWidth/2
+            if(e.clientX<width){
+                effect.current.style.transition=`0s`
+                effect.current.style.transform=`translateX(-100%)`
+            }
+            else{
+                effect.current.style.transition=`0s`
+                effect.current.style.transform=`translateX(200%)`
+            }
+    }
+   const effect=useRef()
 useEffect(()=>{
     let size=slideRef.current.clientWidth
     slideRef.current.style.transform=`translateX(`+ -size +"px"
+})
+useEffect(()=>{
+    window.addEventListener("mousemove",effectSlideFn)
+    return ()=>{
+        window.removeEventListener("mousemove",effectSlideFn)
+    }
 })
 const slideRef=useRef()
 
@@ -26,6 +45,14 @@ let imgSlider=data.map((el,i,arr)=>{
 
     return ( <div className={s.header}>
         <div className={s.carouzel}>
+            <div ref={effect} className={s.effectSlide}>
+                <span className={`${s.sOne} ${s.span}`}></span>
+                <span className={`${s.sTwo} ${s.span}`}></span>
+                <span className={`${s.sThree} ${s.span}`}></span>
+                <span className={`${s.sFour} ${s.span}`}></span>
+                <span className={`${s.sFive} ${s.span}`}></span>
+                <span className={`${s.sSix} ${s.span}`}></span>
+            </div>
         <div ref={slideRef} className={s.slide}>
 <img src={imgSlider[imgSlider.length-1]}></img>
         {imgSlider.map((el,i,arr)=>{
@@ -40,8 +67,8 @@ let imgSlider=data.map((el,i,arr)=>{
 
         </div>
         </div>
-        <button onClick={()=>changeImageInCarouzel(slideRef,"left",imgSlider)} className={`${s.btn} ${s.btnLeft}`}>Left</button>
-        <button onClick={()=>changeImageInCarouzel(slideRef,"right",imgSlider)} className={`${s.btn} ${s.btnRight}`}>Right</button>
+        <button onClick={()=>changeImageInCarouzel(slideRef,"left",imgSlider,effect)} className={`${s.btn} ${s.btnLeft}`}>Left</button>
+        <button onClick={()=>changeImageInCarouzel(slideRef,"right",imgSlider,effect)} className={`${s.btn} ${s.btnRight}`}>Right</button>
 
     </div> );
 }
